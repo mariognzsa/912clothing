73a0a1539b58043c531aa7a0acf50183e43397f4
Mario Gonzalez Santoyo <mario_gnzsa@hotmail.com>
2024-01-30 00:16:00 -0600

    Setup model and class for product handling

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import ProductHandler from "./ProductHandler.js";
+
+let productHandler = new ProductHandler();
+productHandler.getProducts(); // initial setup
+productHandler.addProductsOnContainer("shopContainer"); // add listing to page
