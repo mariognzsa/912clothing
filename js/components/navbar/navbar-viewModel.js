72a3e5f6ac9144f3ce78fb0f364b6bfb264cd4a5
KevinMachainITA <20151626@aguascalientes.tecnm.mx>
2024-02-29 18:03:17 -0600

    New styles to navbar and design responsive

--- js/components/navbar/navbar-viewModel.js
+++ js/components/navbar/navbar-viewModel.js
@@ -18,16 +18,33 @@ class Navbar extends HTMLElement {
         document.addEventListener('scroll', () => {
             const nav = this.root.querySelector('#navbar');
             const links = this.root.querySelectorAll('a');
-        
+            const logo = this.root.querySelector('#navbar_logo');
+            const svgPaths = this.root.querySelectorAll('#menu-svg path');
+            const logoResponsive = this.root.querySelector('#navbar_responsive_logo');
+
             if(window.scrollY > 100){
                 nav.className = 'navbar sticky scrolled';
+                logo.className = 'logo scrolledLogo';
+                logoResponsive.className ='scrolledText';
+
                 links.forEach(element => {
                     element.className = 'navbar-link scrolledText';
                 });
+
+                svgPaths.forEach(path => {
+                    path.setAttribute('stroke', '#000');
+                });
             } else {
                 nav.className = 'navbar sticky';
+                logo.className = 'logo';
+                logoResponsive.className ='navbar-responsive-logo';
+
                 links.forEach(element => {
                     element.className = 'navbar-link';
+                });
+                
+                svgPaths.forEach(path => {
+                    path.setAttribute('stroke', '#fff');
                 });
             }
         });
