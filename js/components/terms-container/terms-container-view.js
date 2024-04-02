b57616116671f5e2c61fdfc65eb82f114f79f40e
Mario Gonzalez Santoyo <mario_gnzsa@hotmail.com>
2024-04-02 01:32:50 -0600

    Added terms static section

---
+++ js/components/terms-container/terms-container-view.js
@@ -0,0 +1,71 @@
+
+const template = document.createElement("template");
+template.innerHTML =`
+<style>
+    @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+    @import url("js/components/terms-container/terms-container-styles.css");
+</style>
+<div class="terms-container">
+    <div class="terms-top-container">
+        NINE TWELVE Terms and Conditions:
+    </div>
+    <div class="terms-bottom-container">
+        <h4>Size Changes:</h4>
+        <li>to. Online Shopping: Customers have a period of two weeks from the date of receipt of the order to request size changes.</li>
+        <li>b. Physical Purchases: Customers have a period of one week from the date of purchase to request size changes in our physical stores.</li>
+        
+        <h4>Refunds:</h4>
+        <li>to. There are no refunds for purchases made online or in physical stores.</li>
+        <li>b. In the event of a return, an online credit or voucher will be issued for future in-store purchases, subject to our return policies.</li>
+        
+        <h4>Exchange Conditions:</h4>
+        <li>to. Garments must be returned in their original condition, unworn and with all tags intact.</li>
+        <li>b. We reserve the right to refuse exchanges if the returned garment is in poor condition due to customer misuse.</li>
+        
+        <h4>Change Process:</h4>
+        <li>to. Customers must contact our customer service to start the exchange process through our Instagram page.</li>
+        <li>b. Shipping costs associated with a size change are the responsibility of the customer, unless the change is due to an error on the part of NINE TWELVE.</li>
+        
+        <h4>Exclusions:</h4>
+        <li>to. Sale, clearance or customized products are excluded from our exchange policy, unless they have manufacturing defects.</li>
+        
+        <h4>Modifications:</h4>
+        <li>to. We reserve the right to modify or update these terms and conditions at any time without prior notice.</li>
+        <li>b. Changes will be effective immediately upon posting on our website.</li>
+        
+        <h4>By making a purchase from <b>NINE TWELVE</b>, customers accept and agree to be bound by these terms and conditions.</h4>
+    </div>
+    <div class="terms-top-container">
+        Términos y Condiciones de NINE TWELVE:
+    </div>
+    <div class="terms-bottom-container">
+        <h4>Cambios de Talla:</h4>
+        <li>a. Compras en Línea: Los clientes tienen un plazo de dos semanas a partir de la fecha de recepción del pedido para solicitar cambios de talla.</li>
+        <li>b. Compras en Físico: Los clientes tienen un plazo de una semana a partir de la fecha de compra para solicitar cambios de talla en nuestras tiendas físicas.</li>
+        
+        <h4>Reembolsos:</h4>
+        <li>a. No se realizan reembolsos por compras realizadas en línea o en tiendas físicas.</li>
+        <li>b. En caso de devolución, se emitirá un crédito en línea o un vale para futuras compras en tiendas físicas, sujeto a nuestras políticas de devolución.</li>
+        
+        <h4>Condiciones de Cambio:</h4>
+        <li>a. Las prendas deben devolverse en su estado original, sin haber sido usadas y con todas las etiquetas intactas.</li>
+        <li>b. Nos reservamos el derecho de negar cambios si la prenda devuelta está en mal estado debido al uso indebido del cliente.</li>
+        
+        <h4>Proceso de Cambio:</h4>
+        <li>a. Los clientes deben comunicarse con nuestro servicio de atención al cliente para iniciar el proceso de cambio por medio de nuestra pagina de instagram.</li>
+        <li>b. Los costos de envío asociados con el cambio de talla son responsabilidad del cliente, a menos que el cambio sea debido a un error por parte de NINE TWELVE.</li>
+        
+        <h4>Exclusiones:</h4>
+        <li>a. Los productos en oferta, liquidación o personalizados están excluidos de nuestra política de cambios, a menos que presenten defectos de fabricación.</li>
+        
+        <h4>Modificaciones:</h4>
+        <li>a. Nos reservamos el derecho de modificar o actualizar estos términos y condiciones en cualquier momento sin previo aviso.</li>
+        <li>b. Los cambios entrarán en vigencia inmediatamente después de su publicación en nuestro sitio web.</li>
+        
+        <h4>Al realizar una compra en <b>NINE TWELVE</b>, los clientes aceptan y se comprometen a cumplir con estos términos y condiciones.</h4>
+    </div>
+</div>
+
+`;
+
+export default template;
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import ProductCard from "./components/product-card/product-card-viewModel.js";
 import ShopContainer from "./components/shop-container/shop-container-viewModel.js";
 import GalleryContainer from "./components/gallery-container/gallery-container-viewModel.js";
+import TermsContainer from "./components/terms-container/terms-container-viewModel.js";
 import Navbar from "./components/navbar/navbar-viewModel.js";
 import Footer from "./components/footer/footer-viewModel.js";
 
@@ -8,6 +9,7 @@ const loadCustomComponents = () => {
     customElements.define("product-card", ProductCard);
     customElements.define("shop-container", ShopContainer);
     customElements.define("gallery-container", GalleryContainer);
+    customElements.define("terms-container", TermsContainer);
     customElements.define("navbar-card", Navbar);
     customElements.define("footer-card", Footer);
 };
